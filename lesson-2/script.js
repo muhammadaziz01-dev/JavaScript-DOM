347c75a8c20914e55286311f45d364e6493bd6d2
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-29 17:02:04 +0300

    lesson-15

---
+++ lesson-2/script.js
@@ -0,0 +1,40 @@
+// let card = document.querySelector(".card");
+// card.style.width = "400px"; 
+// card.style.height = "400px"; 
+// card.style.backgroundColo = "red";
+
+// console.log(card.style);
+
+// Amaliy misolllar
+
+// const student = [
+//     {name: "Aziz" , percent : 96},
+//     {name: "lola" , percent : 84},
+//     {name: "sardor" , percent : 73},
+//     {name: "Kamol" , percent : 79},
+//     {name: "Bek" , percent : 83},
+//     {name: "Jon" , percent : 77},
+// ];
+
+// let arr = student.filter((el) => el.percent >= 74 && el.percent <84);
+
+// console.log(arr);
+
+
+
+///----------------------------------------------
+
+// 2- misol 
+
+// const student = [
+//     {name: "Aziz" , percent : 96},
+//     {name: "lola" , percent : 84},
+//     {name: "sardor" , percent : 73},
+//     {name: "Kamol" , percent : 79},
+//     {name: "Bek" , percent : 83},
+//     {name: "Jon" , percent : 77},
+// ];
+
+// let arr = student.filter((el) => el.name.length >5);
+
+// console.log(arr);
